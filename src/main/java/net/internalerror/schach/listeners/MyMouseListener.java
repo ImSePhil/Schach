71a58b2fabf3477bd148bf5f41edbb1190d59f97
= <philipp.gatzka@gmx.ch>
2021-04-07 16:46:11 +0200

    removed javadoc in MyMouseListener.java

--- src/main/java/net/internalerror/schach/listeners/MyMouseListener.java
+++ src/main/java/net/internalerror/schach/listeners/MyMouseListener.java
@@ -4,9 +4,6 @@ import main.java.net.internalerror.schach.util.Vector2;
 
 import java.awt.event.MouseEvent;
 
-/**
- * The type My mouse listener.
- */
 public class MyMouseListener {
     private final boolean[] buttons;
     private static MyMouseListener self;
@@ -23,78 +20,37 @@ public class MyMouseListener {
         lastRelativePosition = Vector2.ZERO;
     }
 
-    /**
-     * Is button pressed boolean.
-     *
-     * @param button the button
-     * @return the boolean
-     */
     public static boolean isButtonPressed(int button) {
         if (button - 1 > 4) return false;
         return getSelf().buttons[button - 1];
     }
 
-    /**
-     * Gets last absolute click.
-     *
-     * @return the last absolute click
-     */
     public static Vector2 getLastAbsoluteClick() {
         return getSelf().lastAbsoluteClick;
     }
 
-    /**
-     * Gets last relative click.
-     *
-     * @return the last relative click
-     */
     public static Vector2 getLastRelativeClick() {
         return getSelf().lastRelativeClick;
     }
 
-    /**
-     * Gets last absolute position.
-     *
-     * @return the last absolute position
-     */
     public static Vector2 getLastAbsolutePosition() {
         return getSelf().lastAbsolutePosition;
     }
 
-    /**
-     * Gets last relative position.
-     *
-     * @return the last relative position
-     */
     public static Vector2 getLastRelativePosition() {
         return getSelf().lastRelativePosition;
     }
 
-    /**
-     * Gets self.
-     *
-     * @return the self
-     */
     public static MyMouseListener getSelf() {
         if (self == null)
             self = new MyMouseListener();
         return self;
     }
 
-    /**
-     * Mouse clicked.
-     *
-     * @param e the e
-     */
     public static void mouseClicked(MouseEvent e) {
         // Does nothing, because its not needed yet.
     }
 
-    /**
-     * Mouse pressed.
-     *
-     * @param e the e
-     */
     public static void mousePressed(MouseEvent e) {
         int btn = e.getButton() - 1;
         if (btn > 4) return;
@@ -103,50 +59,25 @@ public class MyMouseListener {
         getSelf().lastRelativeClick.set(e.getX(), e.getY());
     }
 
-    /**
-     * Mouse released.
-     *
-     * @param e the e
-     */
     public static void mouseReleased(MouseEvent e) {
         int btn = e.getButton() - 1;
         if (btn > 4) return;
         getSelf().buttons[btn] = false;
     }
 
-    /**
-     * Mouse exited.
-     *
-     * @param e the e
-     */
     public static void mouseExited(MouseEvent e) {
         // Does nothing, because its not needed yet.
     }
 
-    /**
-     * Mouse moved.
-     *
-     * @param e the e
-     */
     public static void mouseMoved(MouseEvent e) {
         getSelf().lastAbsolutePosition.set(e.getXOnScreen(), e.getYOnScreen());
         getSelf().lastRelativePosition.set(e.getX(), e.getY());
     }
 
-    /**
-     * Mouse dragged.
-     *
-     * @param e the e
-     */
     public static void mouseDragged(MouseEvent e) {
         //Does nothing, because its not needed yet.
     }
 
-    /**
-     * Mouse entered.
-     *
-     * @param e the e
-     */
     public static void mouseEntered(MouseEvent e) {
         // Does nothing, because its not needed yet.
     }
