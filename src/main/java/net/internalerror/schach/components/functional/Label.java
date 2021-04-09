6ab3c7d6c0d8c7a97b0b4f5610b016ca3cc7353c
Philipp Gatzka <gatzka@sowatec.com>
2021-04-09 08:50:06 +0200

    added basic Label component

---
+++ src/main/java/net/internalerror/schach/components/functional/Label.java
@@ -0,0 +1,81 @@
+package main.java.net.internalerror.schach.components.functional;
+
+import main.java.net.internalerror.schach.components.Node;
+import main.java.net.internalerror.schach.util.Vector2;
+
+import java.awt.*;
+
+public class Label extends Node {
+    private String text;
+    private Font font;
+    private Color fontColor;
+    private int fontSize;
+    private Vector2 size;
+
+
+    @Override
+    public Label setPosition(Vector2 position) {
+        this.position = position;
+        return this;
+    }
+
+    @Override
+    public void render(Graphics2D graphics2D) {
+        graphics2D.setColor(fontColor);
+        graphics2D.setFont(font);
+        float pivotX = size.getX() / 2 - ((float) graphics2D.getFontMetrics().stringWidth(text)) / 2;
+        float pivotY = size.getY() / 2 + ((float) graphics2D.getFontMetrics().getFont().getSize()) / 2;
+        graphics2D.drawString(text, position.getX() + pivotX, position.getY() + pivotY);
+    }
+
+    @Override
+    public Label setDepth(int depth) {
+        this.depth = depth;
+        return this;
+    }
+
+    public Vector2 getSize() {
+        return size;
+    }
+
+    public Label setSize(Vector2 size) {
+        this.size = size;
+        return this;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Label setText(String text) {
+        this.text = text;
+        return this;
+    }
+
+    public Font getFont() {
+        return font;
+    }
+
+    public Label setFont(Font font) {
+        this.font = font;
+        return this;
+    }
+
+    public Color getFontColor() {
+        return fontColor;
+    }
+
+    public Label setFontColor(Color fontColor) {
+        this.fontColor = fontColor;
+        return this;
+    }
+
+    public int getFontSize() {
+        return fontSize;
+    }
+
+    public Label setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+        return this;
+    }
+}
